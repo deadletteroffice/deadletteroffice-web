b99e32e82fe334b32a420ca85359149335f2e3cc
Tristan Chilvers <tmchilvers@gmail.com>
2024-07-14 18:03:03 -0400

    test youtube embed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div>Your mail is lost but you are not.</div>;
+  return (
+    <div>
+      <iframe
+        width="1236"
+        height="695"
+        src="https://www.youtube.com/embed/?listType=playlist&list=PLGQ9CnKzm5CQB3qqTV6SDIzgz8noy8692"
+        title="Dead Letter Office Live Stream"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+      <div>Your mail is lost but you are not.</div>
+    </div>
+  );
 }
 
 export default App;
